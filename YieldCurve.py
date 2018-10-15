e535f4553548ea377a9f59822537a880d6cbec1c
CodingV3s <44152326+CodingV3s@users.noreply.github.com>
2018-10-15 15:18:25 -0500

    Update YieldCurve.py
    Now you can plot yield curves from 1990 to 2018 on one plot
    or you can select a particular year to plot

--- YieldCurve.py
+++ YieldCurve.py
@@ -1,11 +1,22 @@
 import requests
+import sys
 import pandas as pd
 import xml.etree.cElementTree as ET
 import matplotlib.pyplot as plt
 
-what_year = input("what year would you like the yield curve for ? (1990-2018) :")
+print (' Hi I am Charles, I can plot yield curves from 1990-current year \n')
+print (' Would you like a plot from 1990-current year or would you like plot for a particular year showing 12 month \n')
 
-website = "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%20"+str(what_year)
+year_or_month = input( "Enter 'Y' if you want it for all the year or Enter 'M' if you want it for a particular year : ")
+
+if year_or_month =='Y' or year_or_month =='y':
+    website = "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData"
+elif year_or_month =='M' or year_or_month =='m':
+    what_year = input("what year would you like the yield curve for ? (1990-2018) :")
+    website = "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%20"+str(what_year)
+else:
+    print ('Invalid entry')
+    sys.exit()
 
 resp = requests.get(website)
 
@@ -23,11 +34,11 @@ data = data.fillna(0)
 root = tree.getroot()
 # finding the tag the should be iterated is essential,for a in use root.iter(): print (a.tag, a.attrib) this gives you the right tag
 # here properties is nested inside entry -> content subchilds of the element, but you dont need to specify that just need the right tag
-for a in root.iter('{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}properties'):
-    c= []
-    for b in a:
-        c.append((b.text))
-    data.loc[c[1][0:10]]= c[2:14]
+for element in root.iter('{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}properties'):
+    nested_data= []
+    for child in element:
+        nested_data.append((child.text))
+    data.loc[nested_data[1][0:10]]= nested_data[2:14]
 
 # this had to be done as US government added a new 2 month bond as I was trying to code this thing
 data = data.drop(['2 month'], axis = 1)
@@ -42,12 +53,11 @@ data.index = pd.to_datetime(data.index)
 
 """ Whats being done here is that all the daily data is grouped by month,average and put into a new dataframe
  this allow for the graph to be more easily viewed"""
-monthly = data.groupby(pd.Grouper(freq='M')).mean()
-
-monthly = monthly.T # required cause plot is performed column wise and not row wise
+yearly_monthly = data.groupby(pd.Grouper(freq=year_or_month)).mean()
+yearly_monthly = yearly_monthly.T # required cause plot is performed column wise and not row wise
 
 # this is done to access the properties of the particular plot
-ax = monthly.plot(title = "US Treasury bond rate yield curve", grid = 1)
+ax = yearly_monthly.plot(title = "US Treasury bond rate yield curve", grid = 1)
 
 # sets number of points on the x-axis equal to the number of points in a column
 ax.set_xticks(range(len(new_column)))
